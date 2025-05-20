dc9472bb4520881766cdc3c455d7ae86b0e260d7
Serafim Frolkin <serafimfrolkin@gmail.com>
2025-05-20 10:52:33 +0300

    test

---
+++ src/main/scala/TripData.scala
@@ -0,0 +1,21 @@
+case class TripData(
+                     VendorID: Int,
+                     tpep_pickup_datetime: String,
+                     tpep_dropoff_datetime: String,
+                     passenger_count: Long,
+                     trip_distance: Float,
+                     RatecodeID: Int,
+                     store_and_fwd_flag: String,
+                     PULocationID: Int,
+                     DOLocationID: Int,
+                     payment_type: Int,
+                     fare_amount: Float,
+                     extra: Float,
+                     mta_tax: Float,
+                     tip_amount: Float ,
+                     tolls_amount: Float,
+                     improvement_surcharge: Float,
+                     total_amount: Float,
+                     congestion_surcharge: Float,
+                     Airport_fee:Float
+                   )
--- src/main/scala/homework_11_rdd.scala
+++ src/main/scala/homework_11_rdd.scala
@@ -1,6 +1,8 @@
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession, Row}
+import TripData._
+import org.apache.spark.sql.types._
 
 object homework_11_rdd {
 
@@ -9,8 +11,42 @@ object homework_11_rdd {
     val conf: SparkConf = new SparkConf().setAppName("hw_11_rdd").setMaster("local[*]")
     val sc: SparkContext = new SparkContext(config = conf)
 
-    val rdd = sc.textFile("HW/tripdata.csv")
-    val ds: DataFrame = rdd.to
+    val rdd: RDD[Array[String]] = sc.textFile("HW/tripdata.csv")
+      .map(line => line.split(","))
+      .filter(_(0) !="VendorID")
+    val rowRDD = rdd.map(attr => TripData(
+                                      attr(0).toInt,
+                                      attr(1),
+                                      attr(2),
+                                      attr(3).toLong,
+                                      attr(4).toFloat,
+                                      attr(5).toInt,
+                                      attr(6),
+                                      attr(7).toInt,
+                                      attr(8).toInt,
+                                      attr(9).toInt,
+                                      attr(10).toFloat,
+                                      attr(11).toFloat,
+                                      attr(12).toFloat,
+                                      attr(13).toFloat,
+                                      attr(14).toFloat,
+                                      attr(15).toFloat,
+                                      attr(16).toFloat,
+                                      attr(17).toFloat,
+                                      attr(18).toFloat
+                                  ))
+
+
+    val spark: SparkSession = SparkSession.builder()
+      .config(sc.getConf)
+      .getOrCreate()
+
+    import spark.implicits._
+    val ds: Dataset[TripData] = spark.createDataset(rowRDD)
+    ds.show()
+
+
+
 
 
 
